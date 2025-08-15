77863e6249f5fc0332df19926b0dd9db7708ad72
oluwaseun-odufisan <oluwaseun.odufisan@gmail.com>
2025-08-15 12:16:47 +0100

    py added to dashboard and transactions page

--- app/(main)/dashboard/layout.js
+++ app/(main)/dashboard/layout.js
@@ -4,7 +4,7 @@ import { Suspense } from "react";
 
 export default function Layout() {
     return (
-        <div className="min-h-screen bg-gradient-to-b from-orange-50/20 to-white dark:from-gray-900 dark:to-gray-800">
+        <div className="min-h-screen bg-gradient-to-b from-orange-50/20 to-white dark:from-gray-900 dark:to-gray-800 py-8">
             <div className="container mx-auto px-2 xxs:px-3 sm:px-4 md:px-6 py-10 xxs:py-12 sm:py-16 md:py-20 max-w-7xl">
                 <header className="pt-4 xxs:pt-5 sm:pt-6 pb-3 xxs:pb-4">
                     <h1
--- app/(main)/transaction/layout.js
+++ app/(main)/transaction/layout.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const MainLayout = ({ children }) => {
     return (
-        <div className="min-h-screen bg-gradient-to-b from-orange-50/20 to-white dark:from-gray-900 dark:to-gray-800">
+        <div className="min-h-screen bg-gradient-to-b from-orange-50/20 to-white dark:from-gray-900 dark:to-gray-800 py-8">
             <div className="container mx-auto px-2 xxs:px-3 sm:px-4 md:px-6 py-10 xxs:py-12 sm:py-16 md:py-20 max-w-7xl">
                 {children}
             </div>
